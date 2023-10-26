53bfd4f819ef645981a2bd2c6cd52f9824e4cb62
drangar <drangar@us.ibm.com>
2023-10-26 14:49:53 -0700

    updated install and list s3

--- s3ops/aws_cli_ls.sh
+++ s3ops/aws_cli_ls.sh
@@ -1,6 +1,3 @@
-curl "https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-./aws/install -i /projects/allScripts/aws
 echo "[default]
 aws_access_key_id = $s3_access_key
 aws_secret_access_key = $s3_secret_key 
