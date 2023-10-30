b13d4e825c77aaa259c5aec4ea2692eb2c53f03a
drangar <drangar@us.ibm.com>
2023-10-30 12:49:56 -0700

    changes to aws cli ls and install location

--- s3ops/aws_cli_ls.sh
+++ s3ops/aws_cli_ls.sh
@@ -1,7 +1,8 @@
+echo "AWS CLI has been installed here -> $AWS_CLI"
 echo "[default]
 aws_access_key_id = $s3_access_key
 aws_secret_access_key = $s3_secret_key 
 region = $s3_region 
-endpoint_url = $s3_endpoint " > /projects/allScripts/aws/credentials
-export AWS_CONFIG_FILE=/projects/allScripts/aws/credentials
-/projects/allScripts/aws/v2/current/bin/aws s3 ls $s3_bucket --recursive
+endpoint_url = $s3_endpoint " > aws/credentials
+export AWS_CONFIG_FILE=/aws/credentials
+aws s3 ls $s3_bucket --recursive
