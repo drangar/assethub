bad9a5e73a2aba6fe70960bcdc19cc2c1831f0ad
drangar <drangar@us.ibm.com>
2023-10-30 13:00:19 -0700

    setting up the env vars

--- s3ops/aws_cli_cp.sh
+++ s3ops/aws_cli_cp.sh
@@ -1,11 +1,13 @@
+export AWS_CLI="$(pwd)"
+export PATH=$PATH:$AWS_CLI/aws/v2/current/bin
 echo "[default]
 aws_access_key_id = $s3_access_key
 aws_secret_access_key = $s3_secret_key 
 region = $s3_region 
-endpoint_url = $s3_endpoint " > /projects/allScripts/aws/credentials
-export AWS_CONFIG_FILE=/projects/allScripts/aws/credentials
-LOCAL_DIR="/projects/allScripts/data"
+endpoint_url = $s3_endpoint " > $AWS_CLI/aws/credentials
+export AWS_CONFIG_FILE=$AWS_CLI/aws/credentials
+LOCAL_DIR="$AWS_CLI/data"
 
 for file in $LOCAL_DIR/*; do
-    /projects/allScripts/aws/v2/current/bin/aws s3 cp"$file" s3://$s3_bucket/$file
+    aws s3 cp"$file" s3://$s3_bucket/$file
 done
