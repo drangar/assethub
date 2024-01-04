c01210a63577c8d7ec8c0a2c0414ac2df381fc4c
drangar <drangar@us.ibm.com>
2024-01-04 06:42:53 -0800

    updated fix

--- ocp/list_pods.sh
+++ ocp/list_pods.sh
@@ -1,4 +1,2 @@
-export OC_LOGIN_ENDPOINT="$(ocloginendpoint)"
-export OC_LOGIN_TOKEN="$(oclogintoken)"
-oc login --token=$OC_LOGIN_TOKEN --server=$OC_LOGIN_ENDPOINT
+oc login --token=$oclogintoken --server=$ocloginendpoint
 oc get pods -A
